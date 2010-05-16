9aea71dea4284effc5ac37cf42bbc6832f98e8e7
RiJo <rajoo@mail.com>
2010-05-16 18:29:48 +0200

    initial gallery thumb representing null

--- src/gallery_thumb.php
+++ src/gallery_thumb.php
@@ -21,7 +21,10 @@ if(isset($_GET[GET_KEY_GALLERY_ID])) {
         not_valid_id($id, 'there is no gallery in the database with that id');
     }
 
-    $image = $result[0]['image'];
+    if ($result[0]['image'] == null)
+        $image = generate_null_image();
+    else
+        $image = $result[0]['image'];
     $type = $result[0]['type'];
 
     header("Content-type: " . image_type_to_mime_type($type));
@@ -30,6 +33,39 @@ if(isset($_GET[GET_KEY_GALLERY_ID])) {
 }
 else {
     not_valid_id('', 'no image requested');
+}
+
+function generate_null_image() {
+    // create image canvas
+    if (!$canvas_resource = ImageCreateTrueColor(GALLERY_THUMBNAIL_WIDTH, GALLERY_THUMBNAIL_HEIGHT))
+        die("Failed to create destination image");
+    
+    // set canvas background color
+    $panel_color = str_replace("#", "", GALLERY_THUMBNAIL_PANEL_COLOR);
+    if (strlen($panel_color) != 6)
+        die("Panel color is not properly formatted: #$panel_color");
+    $canvas_r = hexdec(substr($panel_color, 0, 2));
+    $canvas_g = hexdec(substr($panel_color, 2, 2));
+    $canvas_b = hexdec(substr($panel_color, 4, 2));
+    $canvas_bg = imagecolorallocate($canvas_resource, $canvas_r, $canvas_g, $canvas_b);
+    imagefill($canvas_resource, 0, 0, $canvas_bg);
+
+  /*// an email address in a string
+  $string = "email@example.com";
+  // some variables to set
+  $font  = 4;
+  //black text
+  $text_color = imagecolorallocate ($canvas_resource, 0, 0, 0);
+  // put it all together
+  imagestring ($canvas_resource, $font, 0, 0,  $string, $text_color);*/
+
+    // write canvas to file
+    if (!imagejpeg($canvas_resource, IMAGE_TEMP_FILE, IMAGE_THUMBNAIL_QUALITY))
+        die("Could not create new jpeg image");
+
+    imagedestroy($canvas_resource);
+
+    return file_get_contents(IMAGE_TEMP_FILE);
 }
 
 function not_valid_id($id, $message) {
