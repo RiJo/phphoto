b9d469f80a3800bfb7217f76439cc7f45b36e52d
RiJo <rajoo@mail.com>
2010-05-30 19:01:34 +0200

    initial database dump

--- dump.sql
+++ dump.sql
@@ -22,15 +22,15 @@ USE `phphoto`;
 --
 
 CREATE TABLE IF NOT EXISTS `galleries` (
-  `id` int(11) NOT NULL auto_increment,
-  `thumbnail` blob,
-  `title` varchar(255) collate latin1_general_ci NOT NULL,
-  `description` text collate latin1_general_ci,
-  `views` int(11) NOT NULL,
-  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `created` datetime NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `UNIQUE` (`title`)
+    `id` int(11) NOT NULL auto_increment,
+    `thumbnail` blob,
+    `title` varchar(255) collate latin1_general_ci NOT NULL,
+    `description` text collate latin1_general_ci,
+    `views` int(11) NOT NULL,
+    `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    `created` datetime NOT NULL,
+    PRIMARY KEY  (`id`),
+    UNIQUE KEY `UNIQUE` (`title`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci ROW_FORMAT=COMPACT;
 
 -- --------------------------------------------------------
@@ -40,12 +40,12 @@ CREATE TABLE IF NOT EXISTS `galleries` (
 --
 
 CREATE TABLE IF NOT EXISTS `image_to_gallery` (
-  `gallery_id` int(11) NOT NULL,
-  `image_id` int(11) NOT NULL,
-  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `created` datetime NOT NULL,
-  PRIMARY KEY  (`gallery_id`,`image_id`),
-  KEY `image_id` (`image_id`)
+    `gallery_id` int(11) NOT NULL,
+    `image_id` int(11) NOT NULL,
+    `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    `created` datetime NOT NULL,
+    PRIMARY KEY  (`gallery_id`,`image_id`),
+    KEY `image_id` (`image_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
 -- --------------------------------------------------------
@@ -55,22 +55,22 @@ CREATE TABLE IF NOT EXISTS `image_to_gallery` (
 --
 
 CREATE TABLE IF NOT EXISTS `images` (
-  `id` int(11) NOT NULL auto_increment,
-  `data` mediumblob NOT NULL,
-  `thumbnail` blob NOT NULL,
-  `type` int(2) NOT NULL,
-  `width` int(5) NOT NULL,
-  `height` int(5) NOT NULL,
-  `filesize` bigint(20) NOT NULL,
-  `filename` varchar(255) collate latin1_general_ci NOT NULL,
-  `exif` text collate latin1_general_ci,
-  `title` varchar(255) collate latin1_general_ci default NULL,
-  `description` text collate latin1_general_ci,
-  `views` int(11) NOT NULL,
-  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `created` datetime NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `UNIQUE` (`filename`)
+    `id` int(11) NOT NULL auto_increment,
+    `data` mediumblob NOT NULL,
+    `thumbnail` blob NOT NULL,
+    `type` int(2) NOT NULL,
+    `width` int(5) NOT NULL,
+    `height` int(5) NOT NULL,
+    `filesize` bigint(20) NOT NULL,
+    `filename` varchar(255) collate latin1_general_ci NOT NULL,
+    `exif` text collate latin1_general_ci,
+    `title` varchar(255) collate latin1_general_ci default NULL,
+    `description` text collate latin1_general_ci,
+    `views` int(11) NOT NULL,
+    `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+    `created` datetime NOT NULL,
+    PRIMARY KEY  (`id`),
+    UNIQUE KEY `UNIQUE` (`filename`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci ROW_FORMAT=COMPACT;
 
 --
@@ -81,5 +81,5 @@ CREATE TABLE IF NOT EXISTS `images` (
 -- Constraints for table `image_to_gallery`
 --
 ALTER TABLE `image_to_gallery`
-  ADD CONSTRAINT `image_to_gallery_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `galleries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `image_to_gallery_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+    ADD CONSTRAINT `image_to_gallery_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `galleries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    ADD CONSTRAINT `image_to_gallery_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
