b1845e129d23fdcf8fa515dd7db862fb732eb099
RiJo <rajoo@mail.com>
2010-07-10 15:25:45 +0200

    updated database dump file

---
+++ dbdump_1.2.0.sql
@@ -0,0 +1,134 @@
+-- phpMyAdmin SQL Dump
+-- version 3.2.0-beta1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 10, 2010 at 01:26 PM
+-- Server version: 5.0.27
+-- PHP Version: 5.2.9
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+--
+-- Database: `phphoto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cameras`
+--
+
+CREATE TABLE IF NOT EXISTS `cameras` (
+  `model` varchar(255) collate latin1_general_ci NOT NULL,
+  `crop_factor` float default NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`model`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `galleries`
+--
+
+CREATE TABLE IF NOT EXISTS `galleries` (
+  `id` int(11) NOT NULL auto_increment,
+  `thumbnail` blob,
+  `title` varchar(255) collate latin1_general_ci NOT NULL,
+  `description` text collate latin1_general_ci,
+  `views` int(11) NOT NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `UNIQUE` (`title`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci ROW_FORMAT=COMPACT AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL auto_increment,
+  `data` mediumblob NOT NULL,
+  `thumbnail` blob NOT NULL,
+  `type` int(2) NOT NULL,
+  `width` int(5) NOT NULL,
+  `height` int(5) NOT NULL,
+  `filesize` bigint(20) NOT NULL,
+  `filename` varchar(255) collate latin1_general_ci NOT NULL,
+  `exif` text collate latin1_general_ci,
+  `title` varchar(255) collate latin1_general_ci default NULL,
+  `description` text collate latin1_general_ci,
+  `views` int(11) NOT NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `UNIQUE` (`filename`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci ROW_FORMAT=COMPACT AUTO_INCREMENT=18 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_to_gallery`
+--
+
+CREATE TABLE IF NOT EXISTS `image_to_gallery` (
+  `gallery_id` int(11) NOT NULL,
+  `image_id` int(11) NOT NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`gallery_id`,`image_id`),
+  KEY `image_id` (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_to_tag`
+--
+
+CREATE TABLE IF NOT EXISTS `image_to_tag` (
+  `tag_id` int(11) NOT NULL,
+  `image_id` int(11) NOT NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`tag_id`,`image_id`),
+  KEY `image_id` (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) collate latin1_general_ci NOT NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=3 ;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `image_to_gallery`
+--
+ALTER TABLE `image_to_gallery`
+  ADD CONSTRAINT `image_to_gallery_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `image_to_gallery_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `galleries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `image_to_tag`
+--
+ALTER TABLE `image_to_tag`
+  ADD CONSTRAINT `image_to_tag_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `image_to_tag_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
