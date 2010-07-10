fe0330c9de7573e0ac399586859968c7d304967c
RiJo <rajoo@mail.com>
2010-07-10 05:37:30 +0200

    new version of database dump

--- dbdump_1.1.0.sql
+++ dbdump_1.1.0.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 10, 2010 at 03:20 AM
+-- Generation Time: Jul 10, 2010 at 03:38 AM
 -- Server version: 5.0.27
 -- PHP Version: 5.2.9
 
@@ -12,21 +12,6 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `phphoto`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `categories`
---
-
-CREATE TABLE IF NOT EXISTS `categories` (
-  `id` int(11) NOT NULL auto_increment,
-  `name` varchar(255) collate latin1_general_ci NOT NULL,
-  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `created` datetime NOT NULL,
-  PRIMARY KEY  (`id`),
-  UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -74,21 +59,6 @@ CREATE TABLE IF NOT EXISTS `images` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `image_to_category`
---
-
-CREATE TABLE IF NOT EXISTS `image_to_category` (
-  `category_id` int(11) NOT NULL,
-  `image_id` int(11) NOT NULL,
-  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
-  `created` datetime NOT NULL,
-  PRIMARY KEY  (`category_id`,`image_id`),
-  KEY `image_id` (`image_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `image_to_gallery`
 --
 
@@ -101,16 +71,39 @@ CREATE TABLE IF NOT EXISTS `image_to_gallery` (
   KEY `image_id` (`image_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `image_to_tag`
+--
+
+CREATE TABLE IF NOT EXISTS `image_to_tag` (
+  `tag_id` int(11) NOT NULL,
+  `image_id` int(11) NOT NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`tag_id`,`image_id`),
+  KEY `image_id` (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tags`
+--
+
+CREATE TABLE IF NOT EXISTS `tags` (
+  `id` int(11) NOT NULL auto_increment,
+  `name` varchar(255) collate latin1_general_ci NOT NULL,
+  `changed` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `created` datetime NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_general_ci AUTO_INCREMENT=1 ;
+
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `image_to_category`
---
-ALTER TABLE `image_to_category`
-  ADD CONSTRAINT `image_to_category_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `image_to_category_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Constraints for table `image_to_gallery`
@@ -118,3 +111,10 @@ ALTER TABLE `image_to_category`
 ALTER TABLE `image_to_gallery`
   ADD CONSTRAINT `image_to_gallery_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `image_to_gallery_ibfk_1` FOREIGN KEY (`gallery_id`) REFERENCES `galleries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `image_to_tag`
+--
+ALTER TABLE `image_to_tag`
+  ADD CONSTRAINT `image_to_tag_ibfk_2` FOREIGN KEY (`image_id`) REFERENCES `images` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `image_to_tag_ibfk_1` FOREIGN KEY (`tag_id`) REFERENCES `tags` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
