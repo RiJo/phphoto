c829306dfe8b25e1d0b9f5dfc702fcfad474a8e9
RiJo <rajoo@mail.com>
2010-07-11 02:15:14 +0200

    phphoto-1.2.0

--- src/config.php
+++ src/config.php
@@ -6,8 +6,8 @@
  */
 
 define('GALLERY_NAME',                      'phphoto');
-define('GALLERY_VERSION',                   '1.1.0');
-define('GALLERY_DATE',                      '2010-07-10');
+define('GALLERY_VERSION',                   '1.2.0');
+define('GALLERY_DATE',                      '2010-07-11');
 define('GALLERY_DEVELOPERS',                'RiJo');
 
 define('GALLERY_CHARSET',                   'iso-8859-1');
